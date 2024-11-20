44a2df0809dfc9d381714f1de8a7307b1bfd9619
Enric Velasco Bufi <enricvbufi@gmail.com>
2024-11-20 20:57:37 +0100

    Added multiOptions variable
    Added a MultiOptions variable to model.

--- template/models/models.py
+++ template/models/models.py
@@ -8,3 +8,4 @@
 #     name = fields.Char(string="Variable description", required=False)
 #     quantity = fields.Integer(store=False)
 #     relation = fields.Many2one('module.model', string="Variable description", required=True)
+#     multipleOptions = fields.Selection(string="description", selection=[{"1", "Type 1"}, {"2", "Type 2"}, {"3", "Type 3"}, {"4", "Type 4"}], defualt="1")
