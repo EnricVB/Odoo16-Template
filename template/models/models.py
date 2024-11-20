01fc3fe2ecb4c8a08d3efe6b1dea46ab7a0a669f
Enric Velasco Bufi <enricvbufi@gmail.com>
2024-11-20 20:10:51 +0100

    Added Odoo16 Template
    This repository has this templates:
    - models.py: Includes a simple Model with a relation, and required variables.
    - tree_view.xml: Includes a simple Tree View of created models, and a creation page for said model.
    - menus.xml: Includes a list of pages.

---
+++ template/models/models.py
@@ -0,0 +1,10 @@
+# from odoo import models, fields, api
+
+
+# class Model(models.Model):
+#     _name = 'module.model'
+#     _description = 'module.model'
+
+#     name = fields.Char(string="Variable description", required=False)
+#     quantity = fields.Integer(store=False)
+#     relation = fields.Many2one('module.model', string="Variable description", required=True)
